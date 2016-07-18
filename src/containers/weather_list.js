81cc8b948d3de87306a98d8b5ed788469e16fc1c
rayGreycloud <rayGreycloud@gmail.com>
2016-07-18 15:02:09 -0700

    Integrate google maps to display location

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 export class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,11 +9,11 @@ export class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
-    console.log(temps);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="&deg;F"/></td>
         <td><Chart data={pressures} color="green" units="hPa"/></td>
         <td><Chart data={humidities} color="blue" units="%"/></td>
